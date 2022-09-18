228e94421501c4c57b4cfe7a39b9cb21a2b6dac1
Abhishek-Deupa <abhishekdeupa@gmail.com>
2022-09-18 14:58:30 +0530

    Added functionality to refer on WhatsApp

--- app/src/main/java/com/wormoscorp/ieaapp/Refer.java
+++ app/src/main/java/com/wormoscorp/ieaapp/Refer.java
@@ -10,25 +10,24 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.cardview.widget.CardView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
+
 public class Refer extends AppCompatActivity {
 
+    final String status = "In Review";
     EditText nameEdtTxt, company_nameEdtTxt, contact_numberEdtTxt, email_addressEdtTxt;
     AppCompatButton referBackButton, referButton;
     String name, company_name, contact_number;
-    FirebaseAuth mAuth=FirebaseAuth.getInstance();
-    CardView myReferBtn;
-    final String status = "In Review";
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    CardView myReferBtn, referWhatsAppCv;
     ProgressDialog progressDialog;
 
     @Override
@@ -37,7 +36,7 @@ public class Refer extends AppCompatActivity {
         setContentView(R.layout.activity_refer);
 
         progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Refering...");
+        progressDialog.setMessage("Referring...");
 
         referBackButton = findViewById(R.id.refer_back_button);
         referButton = findViewById(R.id.refer_button);
@@ -46,13 +45,15 @@ public class Refer extends AppCompatActivity {
         contact_numberEdtTxt = findViewById(R.id.refer_contact_number_editText);
         email_addressEdtTxt = findViewById(R.id.refer_email_address_editText);
         myReferBtn = findViewById(R.id.my_refer_btn);
+        referWhatsAppCv = findViewById(R.id.refer_whats_app_cv);
 
-        myReferBtn.setOnClickListener(view -> {
-            startActivity(new Intent(Refer.this,My_Refer.class));
-        });
+        myReferBtn.setOnClickListener(view -> startActivity(new Intent(Refer.this, My_Refer.class)));
 
 
         referBackButton.setOnClickListener(view -> finish());
+
+        //referring on WhatsApp
+        referWhatsAppCv.setOnClickListener(view -> referOnWhatsApp());
 
         referButton.setOnClickListener(v -> {
             String email_address;
@@ -68,54 +69,60 @@ public class Refer extends AppCompatActivity {
             } else {
                 progressDialog.show();
                 DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Refer");
-                DatabaseReference databaseReferencebyuser = FirebaseDatabase.getInstance().getReference().child("Refer by Member");
+                DatabaseReference databaseReferenceByUser = FirebaseDatabase.getInstance().getReference().child("Refer by Member");
                 String referKey = databaseReference.push().getKey();
                 name = nameEdtTxt.getText().toString();
                 company_name = company_nameEdtTxt.getText().toString();
                 contact_number = contact_numberEdtTxt.getText().toString();
-                if(email_addressEdtTxt.getText().toString().isEmpty()){
+                if (email_addressEdtTxt.getText().toString().isEmpty()) {
                     email_address = "";
-                }else {
+                } else {
                     email_address = email_addressEdtTxt.getText().toString();
                 }
 
-                Refermodelclass newrefer = new Refermodelclass(name,email_address,contact_number,company_name,mAuth.getCurrentUser().getEmail(),status);
-                databaseReference.child(referKey).setValue(newrefer).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-
-                        databaseReferencebyuser.child(mAuth.getCurrentUser().getEmail().replaceAll("\\.","%7"))
-                                .child(referKey).setValue(newrefer).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void unused) {
-                                        progressDialog.dismiss();
-                                        sendEmail();
-                                    }
-                                }).addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        progressDialog.dismiss();
-                                        Toast.makeText(Refer.this, "Please Try Again", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        progressDialog.dismiss();
-                        Toast.makeText(Refer.this, "Please Try Again", Toast.LENGTH_SHORT).show();
-                    }
+                Refermodelclass newRefer = new Refermodelclass(name, email_address, contact_number, company_name, Objects.requireNonNull(mAuth.getCurrentUser()).getEmail(), status);
+                databaseReference.child(Objects.requireNonNull(referKey)).setValue(newRefer).addOnSuccessListener(unused -> databaseReferenceByUser.child(Objects.requireNonNull(mAuth.getCurrentUser().getEmail()).replaceAll("\\.", "%7"))
+                        .child(referKey).setValue(newRefer).addOnSuccessListener(unused1 -> {
+                            progressDialog.dismiss();
+                            sendEmail();
+                        }).addOnFailureListener(e -> {
+                            progressDialog.dismiss();
+                            Toast.makeText(Refer.this, "Please Try Again", Toast.LENGTH_SHORT).show();
+                        })).addOnFailureListener(e -> {
+                    progressDialog.dismiss();
+                    Toast.makeText(Refer.this, "Please Try Again", Toast.LENGTH_SHORT).show();
                 });
             }
         });
     }
 
+    //function to refer on WhatsApp
+    private void referOnWhatsApp() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        String appUrl = "https://play.google.com/store/apps/details?id=com.wormoscorp.ieaapp"; //PlayStore URL of IEA App
+        String message = "Download IEA - Industrial Entrepreneurs app from PlayStore and give your business a boost.\n\n" + appUrl; //message sent on WhatsApp
+
+        intent.setType("text/plain");
+        intent.setPackage("com.whatsapp");
+
+        intent.putExtra(
+                Intent.EXTRA_TEXT,
+                message);
+
+        // Checking whether Whatsapp is installed or not
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            Toast.makeText(this,"Please install whatsapp first.",Toast.LENGTH_SHORT).show();
+        } else {
+            startActivity(intent);
+        }
+    }
+
     @SuppressLint("IntentReset")
-    protected void sendEmail() {
+    private void sendEmail() {
         name = nameEdtTxt.getText().toString();
         company_name = company_nameEdtTxt.getText().toString();
         contact_number = contact_numberEdtTxt.getText().toString();
-        String email_address = email_addressEdtTxt.getText().toString();
+//        String email_address = email_addressEdtTxt.getText().toString();
         Log.i("Send email", "");
 
         String[] TO = {"wormoscorporation@gmail.com"};
@@ -125,11 +132,7 @@ public class Refer extends AppCompatActivity {
 
         emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "IEA Member Referral");
-        if(email_address.isEmpty()){
-            emailIntent.putExtra(Intent.EXTRA_TEXT, "New Member Referral\n\nThis link is shared by: " + name);
-        } else {
-            emailIntent.putExtra(Intent.EXTRA_TEXT, "New Member Referral\n\nThis link is shared by: " + name);
-        }
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "New Member Referral\n\nThis link is shared by: " + name);
 
         try {
             startActivity(Intent.createChooser(emailIntent, "Send mail..."));
